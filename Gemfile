8f3e0d28126245df00338697ce188504e934a799
Yair Facio <yair.facio11@gmail.com>
2020-02-17 16:00:45 -0600

    Renamed foreign key and defined the association between users and event

--- Gemfile
+++ Gemfile
@@ -23,6 +23,7 @@ gem 'jbuilder', '~> 2.7'
 # gem 'bcrypt', '~> 3.1.7'
 
 gem 'bootstrap-sass'
+gem 'hirb'
 
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_17_193254) do
+ActiveRecord::Schema.define(version: 2020_02_17_214108) do
 
   create_table "events", force: :cascade do |t|
     t.datetime "date"
     t.string "location"
     t.text "description"
-    t.integer "user_id", null: false
+    t.integer "creator_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_events_on_user_id"
+    t.index ["creator_id"], name: "index_events_on_creator_id"
   end
 
   create_table "invitations", force: :cascade do |t|
@@ -40,7 +40,7 @@ ActiveRecord::Schema.define(version: 2020_02_17_193254) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  add_foreign_key "events", "users"
+  add_foreign_key "events", "users", column: "creator_id"
   add_foreign_key "invitations", "events"
   add_foreign_key "invitations", "users"
 end
