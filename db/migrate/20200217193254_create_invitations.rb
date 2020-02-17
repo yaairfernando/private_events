07ead5202dcb95189ff5e46ade522d4afd3da182
Yair Facio <yair.facio11@gmail.com>
2020-02-17 13:34:11 -0600

    Setting up the models

---
+++ db/migrate/20200217193254_create_invitations.rb
@@ -0,0 +1,12 @@
+class CreateInvitations < ActiveRecord::Migration[6.0]
+  def change
+    create_table :invitations do |t|
+      t.integer :recipient_id
+      t.text :description
+      t.references :event, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
