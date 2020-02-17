131bd4be458f986d50634cb4133ee06c0e52fb23
Jaakal <jaak.kivinukk@gmail.com>
2020-02-17 22:16:09 +0200

    Created User controller with create, new, show actions and set initial views for them

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  resources :users, only: [:create, :new, :show]
+  # get 'users/new'
+  # get 'users/create'
+  # get 'users/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
