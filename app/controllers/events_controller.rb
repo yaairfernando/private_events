00382c3eb39f2d6b3a4491b3f1ba510ffa043c6b
Yair Facio <yair.facio11@gmail.com>
2020-02-18 10:00:52 -0600

    Displaying links in the header and creating the new form for events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,17 +1,33 @@
 class EventsController < ApplicationController
+  include SessionsHelper
+  
   def index
     @events = Event.all
     @from_events_controller = true
   end
 
   def new
+    @event = Event.new
   end
 
   def create
+    @event = Event.new(event_params)
+    @event.user_id = current_user.id
+    byebug
+    if event.save
+      flash[:success] = "You have successfuly created a new event!!!"
+      redirect_to @event
+    else
+      render :new
+    end
   end
 
   def show
 
+  end
+
+  def event_params
+    params.require(:event).permit(:location, :date, :time, :description)
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  root 'users#new'
-  get 'login', to: 'sessions#new'
-  post 'login', to: 'sessions#create'
-  get 'logout', to: 'sessions#destroy'
-  resources :users, only: [:new, :create, :show]
+  root 'sessions#new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
+  get '/signup', to: 'users#new'
+  resources :users, only: [:create, :show]
   resources :events, only: [:index, :new, :create, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
