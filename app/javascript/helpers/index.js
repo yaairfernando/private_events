ecf13946d8b1bc0598398af2776c3d79cc123b13
Yair Facio <yair.facio11@gmail.com>
2020-03-02 08:56:46 -0600

    Adding a image to the events

--- app/javascript/helpers/index.js
+++ app/javascript/helpers/index.js
@@ -1,5 +1,5 @@
 export function getMetaValue(name) {
-  const element = findElement(document.head, `meta[name"${name}"]`);
+  const element = findElement(document.head, `meta[name="${name}"]`);
   if (element) {
     return element.getAttribute("content");
   }
